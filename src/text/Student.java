253962ce9734e82b18eb442027166c39f97df5d7
Hiro <3403585110@qq.com>
2023-06-15 11:27:15 +0800

    第三次提交：学生类添加了全参构造

--- src/text/Student.java
+++ src/text/Student.java
@@ -4,6 +4,11 @@ public class Student {
     private String name;
     private Integer age;
 
+    public Student(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
     public String getName() {
         return name;
     }
