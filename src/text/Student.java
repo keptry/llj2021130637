235a8b9cc0f0855b922b13435fa94568dbf8f570
Hiro <3403585110@qq.com>
2023-06-15 11:08:17 +0800

    第二次提交：编写了学生类，测试了Getter和Setter方法

--- src/text/Student.java
+++ src/text/Student.java
@@ -1,4 +1,31 @@
 package text;
 
 public class Student {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("吕柳俊");
+        student.setAge(20);
+        System.out.println(student.getName());
+        System.out.println(student.getAge());
+
+    }
 }
