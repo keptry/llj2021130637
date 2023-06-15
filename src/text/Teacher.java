b3a89b47881275c3940b0fb9ebe8f3f19bf940e3
Hiro <3403585110@qq.com>
2023-06-15 11:32:10 +0800

    第五次提交：创建了老师类，实现添加临时学生的功能

---
+++ src/text/Teacher.java
@@ -0,0 +1,39 @@
+package text;
+
+public class Teacher {
+    private String name;
+    private Integer age;
+    private Student student;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public static void main(String[] args) {
+        Teacher teacher = new Teacher();
+        // 添加学生信息
+        teacher.setStudent(new Student("AKI", 26));
+        // 获取学生信息
+        System.out.println(teacher.getStudent().getName() + ":" + teacher.getStudent().getAge());
+    }
+}
